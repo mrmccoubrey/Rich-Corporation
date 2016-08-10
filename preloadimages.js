ea6d3c76950ef329f2942375a66063fe3fac7674
mrmccoubrey <mr.mccoubrey@gmail.com>
2016-08-10 00:11:49 -0700

    Preload Images
    Preloads images to smoothen homepage slideshow.

---
+++ preloadimages.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+$.fn.preload = function() {
+    this.each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
+
+// Usage:
+
+$(['background12.jpg','background22.jpg','background32.jpg','background42.jpg','background52.jpg','background62.jpg']).preload();
