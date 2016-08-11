ee25a044f8db83154b414cfeee744508ea16a516
mrmccoubrey <mr.mccoubrey@gmail.com>
2016-08-10 23:39:53 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-                  $('.divnav a').hover(function() {
+                  $('.divnav').hover(function() {
                                    $(this).animate({ color: "red" }, 20);
                                    },function() {
                                    $(this).animate({ color: "white" }, 20);
